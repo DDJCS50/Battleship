3e7556d2f276592d5eb7a747823ee8e44443425d
djns <130430285+DDJCS50@users.noreply.github.com>
2024-04-11 08:59:49 -0700

    Modify tests
    Removed tests from main.js and added new tests to
    ship test file in order to properly test the ship
    methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,6 @@
 import { shipFactory } from "./ship.js";
 
 let shipOne = shipFactory(3, 0, false);
-console.log(shipOne);
 shipOne.hit(shipOne);
-console.log(shipOne);
 shipOne.hit(shipOne);
-console.log(shipOne);
 shipOne.hit(shipOne);
-shipOne.isSunk(shipOne);
-console.log(shipOne);
--- test/ship.test.js
+++ test/ship.test.js
@@ -10,4 +10,18 @@ describe("ship suite", () => {
       sunk: false,
     });
   });
+  test("sinks ship at maximum hits", () => {
+    const mockShip = mockShipBuilder(3, 0, false);
+    mockShip.hit(mockShip);
+    mockShip.hit(mockShip);
+    mockShip.hit(mockShip);
+    mockShip.isSunk(mockShip);
+    expect(mockShip).toMatchObject({
+      length: 3,
+      hits: 3,
+      sunk: true,
+    });
+    expect(mockShip.sunk).toBe(true);
+    expect(mockShipBuilder.mock.calls).toHaveLength(2);
+  });
 });

30f60cb2fe34ddfd2763f8267b8af1e77f8e3baa
djns <130430285+DDJCS50@users.noreply.github.com>
2024-05-03 19:37:34 -0700

    Add func for verifying locations
    Fleshed out validLocation func so that it checks
    the proposed squares that the ship will inhabit
    in order to see if they are free of hits or other
    ships. Added a check to the placeShip func so that
    any initial location instantiated would be
    rejected if it was off the board

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -17,25 +17,106 @@ export function gameboardFactory(board) {
   }
   board = tempArr;
   function placeShip(board, location) {
+    if (
+      location[0] > 9 ||
+      location[0] < 0 ||
+      location[1] > 9 ||
+      location[1] < 0
+    ) {
+      console.log("invalid location input");
+      return;
+    }
     let nose = location;
-    let tempShip = shipFactory(5, 0, false);
+    let tempShip = shipFactory(3, 0, false);
     tempShip.shipOrientation(tempShip, "north");
     let direction = tempShip.shipDirection;
-    for (let i = 0; i < tempShip.length; i++) {
-      if (direction == "north") {
-        tempShip.locationArray.push([nose[0], nose[1] - i]);
-      } else if (direction == "south") {
-        tempShip.locationArray.push([nose[0], nose[1] + i]);
-      } else if (direction == "east") {
-        tempShip.locationArray.push([nose[0] - i, nose[1]]);
-      } else if (direction == "west") {
-        tempShip.locationArray.push([nose[0] + i, nose[1]]);
+    let validity = true;
+    validity = validLocation(board.board, tempShip, nose);
+    if (validity == true) {
+      for (let i = 0; i < tempShip.length; i++) {
+        if (direction == "north") {
+          tempShip.locationArray.push([nose[0], nose[1] - i]);
+        } else if (direction == "south") {
+          tempShip.locationArray.push([nose[0], nose[1] + i]);
+        } else if (direction == "east") {
+          tempShip.locationArray.push([nose[0] - i, nose[1]]);
+        } else if (direction == "west") {
+          tempShip.locationArray.push([nose[0] + i, nose[1]]);
+        }
       }
     }
     return tempShip;
   }
 
-  function validLocation(board) {}
+  function validLocation(board, ship, location) {
+    let nose = location;
+    let validTallies = 0;
+
+    for (let i = 0; i < ship.length; i++) {
+      if (ship.shipDirection == "north") {
+        board.forEach((square) => {
+          if (
+            square.location[0] == nose[0] &&
+            square.location[1] == nose[1] - i
+          ) {
+            if (square.hit == true || square.shipPresent == true) {
+              console.log("invalid");
+            } else {
+              console.log("valid");
+              validTallies++;
+            }
+          }
+        });
+      } else if (ship.shipDirection == "south") {
+        board.forEach((square) => {
+          if (
+            square.location[0] == nose[0] &&
+            square.location[1] == nose[1] + i
+          ) {
+            if (square.hit == true || square.shipPresent == true) {
+              console.log("invalid");
+            } else {
+              console.log("valid");
+              validTallies++;
+            }
+          }
+        });
+      } else if (ship.shipDirection == "east") {
+        board.forEach((square) => {
+          if (
+            square.location[0] == nose[0] - i &&
+            square.location[1] == nose[1]
+          ) {
+            if (square.hit == true || square.shipPresent == true) {
+              console.log("invalid");
+            } else {
+              console.log("valid");
+              validTallies++;
+            }
+          }
+        });
+      } else if (ship.shipDirection == "west") {
+        board.forEach((square) => {
+          if (
+            square.location[0] == nose[0] + i &&
+            square.location[1] == nose[1]
+          ) {
+            if (square.hit == true || square.shipPresent == true) {
+              console.log("invalid");
+            } else {
+              console.log("valid");
+              validTallies++;
+            }
+          }
+        });
+      }
+    }
+    if (validTallies == ship.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   function receiveAttack() {}
   return {
--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,13 @@ shipOne.hit(shipOne);
 shipOne.hit(shipOne);
 let boardOne = gameboardFactory();
 console.log(boardOne.board);
-boardOne.board.map((locationObject) => {
-  console.log(locationObject.hit);
-});
-shipOne.generateShipLocation(shipOne);
-let shipTwo = boardOne.placeShip(boardOne, [5, 5]);
-console.log(shipTwo);
-console.log(boardOne.board[0].location);
+// boardOne.board.map((locationObject) => {
+//   console.log(locationObject.hit);
+// });
+// shipOne.generateShipLocation(shipOne);
+// let shipTwo = boardOne.placeShip(boardOne, [10, 10]);
+// boardOne.board[33].shipPresent = true;
+let shipThree = boardOne.placeShip(boardOne, [3, 3]);
+// console.log(shipTwo);
+console.log(shipThree);
+// console.log(boardOne.board[0].location);
